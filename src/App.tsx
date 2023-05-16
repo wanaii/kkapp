16b7b62e4712867384494c0dd63e8cc904d9ed27
wanaii1018@gmail.com <wanaii1018@gmail.com>
2023-05-16 22:42:54 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>KKKKKK</p>
+        <p>KKKKKK222</p>
         <a
           className="App-link"
           href="https://reactjs.org"
